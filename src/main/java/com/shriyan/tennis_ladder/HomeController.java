09e5ca8d9b97594032b3e780023bd1efacbe5b8f
shriyandagg <shriyand14@gmail.com>
2026-08-24 23:39:47 -0500

    feat: render ladder players from Java

--- src/main/java/com/shriyan/tennis_ladder/HomeController.java
+++ src/main/java/com/shriyan/tennis_ladder/HomeController.java
@@ -1,13 +1,25 @@
 package com.shriyan.tennis_ladder;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String showHomePage() {
+    public String showHomePage(Model model) {
+        List<String> players = List.of(
+            "Alex",
+            "Maya",
+            "Shriyan",
+            "Jordan"
+        );
+
+        model.addAttribute("players", players);
+
         return "home";
     }
 }
