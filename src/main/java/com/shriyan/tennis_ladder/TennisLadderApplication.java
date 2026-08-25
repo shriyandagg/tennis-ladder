239b451e8222d6ffad480f04d643b8c9421ed671
shriyandagg <shriyand14@gmail.com>
2026-08-24 23:37:06 -0500

    feat: create initial tennis ladder webpage

---
+++ src/main/java/com/shriyan/tennis_ladder/TennisLadderApplication.java
@@ -0,0 +1,13 @@
+package com.shriyan.tennis_ladder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisLadderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisLadderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shriyan/tennis_ladder/TennisLadderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shriyan.tennis_ladder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisLadderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
